ae6bc09ab2da8bd41b751c04296ce22b0caf312a
Ken Lâm <hoangvu23108@gmail.com>
2021-08-11 00:45:06 +0700

    Solve Q6 - XOR List

--- DailyKoding.playground/Contents.swift
+++ DailyKoding.playground/Contents.swift
@@ -1,6 +1,17 @@
-import UIKit
+import Foundation
 
-let pair = Q5_20210809.cons(a: 3, b: 4)
+var list = Q6_20210810.XorList()
 
-Q5_20210809.car(pair)
-Q5_20210809.cdr(pair)
+list.add(0)
+list.add(1)
+list.add(2)
+list.add(3)
+list.add(4)
+list.add(5)
+list.add(6)
+
+
+list.get(2)?.value
+list.get(3)?.value
+list.get(6)?.value
+list.get(0)?.value
--- DailyKoding.playground/Sources/Q6_20210810.swift
+++ DailyKoding.playground/Sources/Q6_20210810.swift
@@ -6,4 +6,68 @@ import Foundation
 
 
 public class Q6_20210810: NSObject {
+  public class XorList {
+    var head: UnsafeMutablePointer<XorNode>? = nil
+    var tail: UnsafeMutablePointer<XorNode>? = nil
+    var length = 0
+    
+    public init() {}
+    
+    public func add(_ value: Int) {
+      let newSlice = allocate(value)
+      if head != nil {
+        newSlice.pointee.npxAddr = xor(tail, nil)
+        tail?.pointee.npxAddr = xor(newSlice, xor(tail?.pointee.npxAddr, nil))
+        tail = newSlice
+      } else {
+        tail = newSlice
+        head = newSlice
+      }
+      length += 1
+    }
+    
+    public func get(_ index: Int) -> XorNode? {
+      guard index < length, index >= 0, length > 0 else { return nil }
+      
+      var currentIndex = 0
+      var currentNode = head
+      var prevNode, nextNode: UnsafeMutablePointer<XorNode>?
+      while currentIndex != index {
+        next(&prevNode, &currentNode, &nextNode)
+        currentIndex += 1
+      }
+      
+      return currentNode?.pointee
+    }
+    
+    func xor(_ predecessor: UnsafeMutablePointer<XorNode>?, _ successor: UnsafeMutablePointer<XorNode>?) -> UnsafeMutablePointer<XorNode>? {
+      let next = UInt(bitPattern: predecessor) ^ UInt(bitPattern: successor)
+      return UnsafeMutablePointer(bitPattern: next)
+    }
+    
+    func allocate(_ value: Int) -> UnsafeMutablePointer<XorNode> {
+      let slice = UnsafeMutablePointer<XorNode>.allocate(capacity: 1)
+      slice.initialize(to: XorNode(value: value))
+      return slice
+    }
+    
+    internal func next(
+      _ prev: inout UnsafeMutablePointer<XorNode>?,
+      _ curr: inout UnsafeMutablePointer<XorNode>?,
+      _ next: inout UnsafeMutablePointer<XorNode>?
+      ) {
+      next = xor(prev, curr?.pointee.npxAddr)
+      prev = curr
+      curr = next
+    }
+  }
+  
+  public class XorNode {
+    var npxAddr: UnsafeMutablePointer<XorNode>? = nil
+    public var value: Int
+    
+    public init(value: Int) {
+      self.value = value
+    }
+  }
 }
