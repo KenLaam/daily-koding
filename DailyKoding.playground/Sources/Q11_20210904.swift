b5fec3a68a3ebf9029dd4e0418cbe194b783e85b
Ken Lâm <hoangvu23108@gmail.com>
2021-09-04 16:38:44 +0700

    Improve Q11 - Implementation with Trie concept

--- DailyKoding.playground/Sources/Q11_20210904.swift
+++ DailyKoding.playground/Sources/Q11_20210904.swift
@@ -5,109 +5,89 @@ import Foundation
 /// Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries.
 
 public class Q11_20210904: NSObject {
-  public static func findStringIn(_ array: [String], query: String) -> [String] {
-    let tree = Tree()
-    tree.addStrings(array)
-    
+  public static func findStringIn(_ words: [String], query: String) -> [String] {
+    let tree = Trie(words)
     return tree.find(query)
   }
   
-  class Tree {
-    var roots: [Node] = []
+  class Trie {
+    var root: Node
     
-    func addStrings(_ array: [String]) {
-      for string in array {
-        addNew(string: string)
-      }
-    }
-    
-    func addNew(string: String) {
-      if string.isEmpty {
-        return
-      }
-      var currentNode: Node? = nil
-      for char in string {
-        if currentNode != nil {
-          currentNode = currentNode?.addChild(char)
-        } else if let node = findInRoot(char) {
-          currentNode = node
-        } else {
-          currentNode = Node(char)
-          roots.append(currentNode!)
-        }
-      }
+    init(_ words: [String]) {
+      root = Node()
+      words.forEach { root.insert($0) }
     }
     
     func find(_ query: String) -> [String] {
       var currentNode: Node? = nil
       for q in query {
         if currentNode == nil {
-          currentNode = findInRoot(q)
+          currentNode = root.children[q]
         } else {
-          currentNode = currentNode?.findChild(q)
+          currentNode = currentNode?.children[q]
         }
       }
       return currentNode?.allStrings() ?? []
     }
-    
-    func findInRoot(_ char: Character) -> Node? {
-      return roots.first(where: { $0.char == char })
-    }
-  }
-  
-  class Node {
-    let char: Character
-    var parent: Node?
-    var children: [Node]
-    
-    init(_ value: Character, parent: Node? = nil) {
-      self.char = value
-      self.children = []
-      self.parent = parent
-    }
-    
-    var isLeaf: Bool {
-      parent != nil && children.isEmpty
-    }
-    
-    func addChild(_ char: Character) -> Node {
-      if let existChild = children.first(where: { $0.char == char }) {
-        return existChild
+     
+    class Node {
+      let char: Character?
+      var parent: Node?
+      var children: [Character: Node]
+      
+      init(_ char: Character? = nil, parent: Node? = nil) {
+        self.char = char
+        self.children = [:]
+        self.parent = parent
       }
-      let newChild = Node(char, parent: self)
-      children.append(newChild)
-      return newChild
-    }
-    
-    func findChild(_ char: Character) -> Node? {
-      return children.first { $0.char == char }
-    }
-    
-    func childExist(_ char: Character) -> Bool {
-      children.map{$0.char}.contains(char)
-    }
-    
-    func stringFromRoot() -> String {
-      guard let parent = parent else {
-        return String(char)
+      
+      var isLeaf: Bool {
+        parent != nil && children.isEmpty
       }
-      return parent.stringFromRoot() + String(char)
-    }
-    
-    func findLeaves() -> [Node] {
-      var leaves: [Node] = []
-      for node in children {
-        if node.isLeaf {
-          leaves.append(node)
-        } else {
-          leaves.append(contentsOf: node.findLeaves())
+      
+      func insert(_ word: String) {
+        if word.isEmpty {
+          return
+        }
+        let firstChar = word.first!
+        var child = children[firstChar]
+        if child == nil {
+          child = Node(firstChar, parent: self)
+          children[firstChar] = child
+        }
+        
+        if word.count > 1 {
+          let remainingWord = String(word.dropFirst())
+          child?.insert(remainingWord)
         }
       }
-      return leaves
-    }
-    
-    func allStrings() -> [String] {
-      return findLeaves().map{ $0.stringFromRoot() }
+      
+      func stringFromRoot() -> String {
+        if let char = char {
+          if parent == nil {
+            return String(char)
+          } else {
+            return (parent?.stringFromRoot() ?? "") + String(char)
+          }
+        }
+        return ""
+      }
+      
+      func findLeaves() -> [Node] {
+        var leaves: [Node] = []
+        for (_, node) in children {
+          if node.isLeaf {
+            leaves.append(node)
+          } else {
+            leaves.append(contentsOf: node.findLeaves())
+          }
+        }
+        return leaves
+      }
+      
+      func allStrings() -> [String] {
+        return findLeaves().map{ $0.stringFromRoot() }
+      }
     }
   }
 }
